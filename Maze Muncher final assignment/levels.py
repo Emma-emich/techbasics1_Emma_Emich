794c159a8cf1df453936f3a06898d56e0ae1b32a
Emma-emich <emma.emich@stud.leuphana.de>
2026-09-10 13:13:00 +0200

    Create levels.py
    added levels.py

---
+++ Maze Muncher final assignment/levels.py
@@ -0,0 +1,19 @@
+BEGINNER_LEVELS = [
+    {
+        "maze": [
+            "#########"
+            "#.o.o.o.#"
+            "#.###.#.#"
+            "#.o.#.o.#"
+            "#.#.#.#.#"
+            "#.o.o.o.#"
+            "#########"
+        ],
+        "player_start": (1,1),
+        "enemies": [
+            {"start": (3,5), "moves_every_turn". False},
+        ],
+
+    },
+]
+
